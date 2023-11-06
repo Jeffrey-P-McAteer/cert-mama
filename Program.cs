d8306bc584bd5397da63caa42bd768abd0919687
Jeffrey <jeffrey@jmcateer.com>
2023-11-06 00:20:41 -0500

    Initial foray into Settings.settings nonsense

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cert_mama.Properties;
+using System;
 
 namespace CertMama // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,11 @@ namespace CertMama // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! URLS = " + Settings.Default.URLsToCheckTextFileMonth );
+            Console.WriteLine("Set URLsToCheckTextFileMonth: ");
+            Settings.Default.URLsToCheckTextFileMonth = Console.ReadLine();
+            Settings.Default.Save();
+
         }
     }
 }
